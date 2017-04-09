6614cb76f12949918d685b32f7040fb999cb7226
zhuo <hcxyzlm@126.com>
2017-04-09 22:53:59 +0800

    添加注释

--- ZLMTools/Category/ObjcRuntime/NSObject+Introsection.h
+++ ZLMTools/Category/ObjcRuntime/NSObject+Introsection.h
@@ -15,6 +15,7 @@
 // 所有类方法
 + (NSArray *)classMethods;
 
+// 所有实例方法
 + (NSArray *)instanceMethods;
 
 // 所有成员属性，共有私有属性，以及扩展属性
@@ -26,6 +27,7 @@
 
 + (NSArray *)protocols;
 
+// 获取成员变量
 - (NSArray *)IvarList;
 
 @end
